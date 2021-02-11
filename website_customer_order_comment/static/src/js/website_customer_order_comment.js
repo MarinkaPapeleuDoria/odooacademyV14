5eb1254eb2234e478ae4aa1f85c121d0b22b42e1
mpapeleu <mpapeleu@erpopen.nl>
2021-02-11 21:33:40 +0100

    fix js

--- website_customer_order_comment/static/src/js/website_customer_order_comment.js
+++ website_customer_order_comment/static/src/js/website_customer_order_comment.js
@@ -11,7 +11,7 @@ odoo.define('website_customer_order_comment.payment', function(require) {
             ajax.jsonRpc('/shop/customer_comment/', 'call', {
                 'comment': customer_comment
             var client_order_ref = $('#client_order_ref').val();
-            ajax.jsonRpc('/shop/client_order_ref/', 'call', {
+            ajax.jsonRpc('/shop/customer_comment/', 'call', {
                 'reference': client_order_ref
             })
         });
