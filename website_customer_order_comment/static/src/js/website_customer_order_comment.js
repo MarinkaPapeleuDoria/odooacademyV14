d2b3e4b206302e2e48f2e4a5c1bc3b46c3831495
mpapeleu <mpapeleu@erpopen.nl>
2021-02-11 22:02:10 +0100

    poging 5 js

--- website_customer_order_comment/static/src/js/website_customer_order_comment.js
+++ website_customer_order_comment/static/src/js/website_customer_order_comment.js
@@ -8,14 +8,17 @@ odoo.define('website_customer_order_comment.payment', function(require) {
         $("button#o_payment_form_pay").bind("click", function(ev) {
             var customer_comment = $('#customer_comment').val();
             ajax.jsonRpc('/shop/customer_comment/', 'call', {
-                'reference': client_order_ref,
                 'comment' : customer_comment,
             });
-           /* var customer_comment = $('#customer_comment').val();
-            ajax.jsonRpc('/shop/customer_comment/', 'call', {
-                'comment': customer_comment
-            });*/
-
+        });
+    });
+    $(document).ready(function() {
+        'use strict';
+        $("button#o_payment_form_pay").bind("click", function(ev) {
+            var client_order_ref = $('#client_order_ref').val();
+            ajax.jsonRpc('/shop/client_order_ref/', 'call', {
+                'reference' : client_order_ref,
+            });
         });
     });
 });
